2fd70016efa721da2ed357db85d9616b8b5ff828
tm0765 <tomwebb994@proton.me>
2025-12-17 08:01:30 +0000

    Update me.sh

--- me.sh
+++ me.sh
@@ -7,5 +7,5 @@ python_script="scanner.py"
 # Read the file line by line
 while IFS= read -r line; do
     # Execute the Python script with the line as an argument
-    python3 "$python_script" "$line"
+    python3 "$python_script" "-u" "$line"
 done < "$input_file"
